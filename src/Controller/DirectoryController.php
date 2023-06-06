0b8e5295a57748ec1bccb78a6f212de52e265fee
kot_mech <babecaleksei@yandex.ru>
2023-06-06 22:08:07 +0700

    added getUsersDirRoute

--- src/Controller/DirectoryController.php
+++ src/Controller/DirectoryController.php
@@ -5,15 +5,21 @@ namespace App\Controller;
 use App\Attribute\RequestBody;
 use App\Model\CreateDirectoryRequest;
 use App\Model\DirContentListResponse;
+use App\Repository\DirectoryRepository;
 use App\Service\DirectoryService;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DirectoryController extends AbstractController
 {
-    public function __construct(private DirectoryService $directoryService)
+    public function __construct(
+        private DirectoryService $directoryService,
+        private Security $security,
+        private DirectoryRepository $directoryRepository
+    )
     {
     }
 
@@ -46,5 +52,21 @@ class DirectoryController extends AbstractController
     public function getContentFromDir(int $dirId): DirContentListResponse
     {
         return $this->directoryService->getDirContent($dirId);
+    }
+
+    /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Return user`s directory content",
+     *
+     *     @Model(type=DirContentListResponse::class)
+     * )
+     */
+    #[Route(path: '/api/v1/directories/getUsersDir', methods: ['GET'])]
+    public function getUsersDir(): DirContentListResponse
+    {
+        $user = $this->security->getUser();
+        $dir = $this->directoryRepository->findOneBy(['name' => $user->getUserIdentifier(), 'user' => $user]);
+        return $this->directoryService->getDirContent($dir->getId());
     }
 }
